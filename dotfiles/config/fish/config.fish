78486afda5e4e070fcbec1e0e9e283c1928fb4d5
Tommy Crumrine <tommy@traveljoy.com>
2024-01-30 09:46:36 -0600

    updates

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set -x NVM_DIR ~/.nvm
+set -x EDITOR nvim
 nvm use default --silent
 alias vim nvim
-
+alias vi nvim
+status --is-interactive; and rbenv init - fish | source
