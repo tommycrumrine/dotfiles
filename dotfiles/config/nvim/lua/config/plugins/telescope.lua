a68e004a5ca7aee1779e447a97ec1616de5acffb
Tommy Crumrine <tommy@traveljoy.com>
2024-02-04 20:22:12 -0600

    Telescope grep args

--- dotfiles/config/nvim/lua/config/plugins/telescope.lua
+++ dotfiles/config/nvim/lua/config/plugins/telescope.lua
@@ -3,18 +3,41 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = true,
-      override_file_sorter = true,
-      case_mode = "smart_case",
-    }
+    "nvim-telescope/telescope-live-grep-args.nvim",
   },
   config = function()
+    local telescope = require("telescope")
+    local lga_actions = require("telescope-live-grep-args.actions")
+    local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+    
+    telescope.setup {
+      extensions = {
+        live_grep_args = {
+          auto_quoting = true, -- enable/disable auto-quoting
+          -- define mappings, e.g.
+          mappings = { -- extend mappings
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+          },
+          -- ... also accepts theme settings, for example:
+          -- theme = "dropdown", -- use dropdown theme
+          -- theme = { }, -- use own theme spec
+          -- layout_config = { mirror=true }, -- mirror preview pane
+        }
+      },
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = "smart_case",
+      },
+    }
+    telescope.load_extension("live_grep_args");
+
     vim.keymap.set("n", "<C-f>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    vim.keymap.set("n", "<C-g>g", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    vim.keymap.set("n", "<C-g><C-g>", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    vim.keymap.set("n", "<C-g>g", telescope.extensions.live_grep_args.live_grep_args, { desc = "Find string in cwd" })
+    vim.keymap.set("n", "<C-g><C-g>", live_grep_args_shortcuts.grep_word_under_cursor, { desc = "Find string under cursor in cwd" })
   end,
 }
