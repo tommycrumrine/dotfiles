9e18ec70bec4197ccd2df33cfc1066301498dc6c
Tommy Crumrine <tommy@traveljoy.com>
2024-04-05 09:36:57 -0500

    updates

--- dotfiles/config/nvim/lua/config/plugins/vim-prettier.lua
+++ dotfiles/config/nvim/lua/config/plugins/vim-prettier.lua
@@ -1,1 +1,14 @@
-return { "prettier/vim-prettier" }
+return {
+  "prettier/vim-prettier",
+  config = function()
+    vim.cmd [[
+      : let g:prettier#autoformat = 1
+      : let g:prettier#autoformat_require_pragma = 0
+      : augroup Prettier
+      :   autocmd!
+      :   autocmd BufWritePre *.js,*.jsx,*.mjs,*.ts,*.tsx,*.css,*.less,*.scss,*.json,*.graphql,*.gql,*.markdown,*.md,*.mdown,*.mkd,*.mkdn,*.mdx,*.vue,*.svelte,*.yml,*.yaml,*.html,*.xml noautocmd call prettier#Autoformat()
+      : augroup end
+    ]]
+  end,
+
+}
