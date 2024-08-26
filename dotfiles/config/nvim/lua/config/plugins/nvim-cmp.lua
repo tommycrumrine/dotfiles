e49648c32693db10e739073329110608e53bb254
Tommy Crumrine <tommy@traveljoy.com>
2024-08-26 09:42:53 -0500

    updating for codeium

--- dotfiles/config/nvim/lua/config/plugins/nvim-cmp.lua
+++ dotfiles/config/nvim/lua/config/plugins/nvim-cmp.lua
@@ -33,12 +33,13 @@ return {
         --["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
         --["<C-b>"] = cmp.mapping.scroll_docs(-4),
         --["<C-f>"] = cmp.mapping.scroll_docs(4),
-        --["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         --["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        --["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<C-c>"] = cmp.mapping.complete(), -- show completion suggestions
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
+        { name = "codeium" },
         { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
