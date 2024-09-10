e8592f363614ba50dcb83417837e74b5c141886d
Tommy Crumrine <tommy@traveljoy.com>
2024-09-10 12:31:20 -0600

    lots of updates and using copilot instead of codeium for now

--- dotfiles/config/nvim/lua/config/plugins/vim-rspec.lua
+++ dotfiles/config/nvim/lua/config/plugins/vim-rspec.lua
@@ -1,1 +1,8 @@
-return { "thoughtbot/vim-rspec" }
+return {
+  "thoughtbot/vim-rspec",
+  config = function()
+    vim.keymap.set("n", "<C-s>t", "<cmd>RunCurrentSpecFile<cr>")
+    vim.keymap.set("n", "<C-s>s", "<cmd>RunNearestSpec<cr>")
+    vim.keymap.set("n", "<C-s>l", "<cmd>RunLastSpec<cr>")
+  end,
+}
